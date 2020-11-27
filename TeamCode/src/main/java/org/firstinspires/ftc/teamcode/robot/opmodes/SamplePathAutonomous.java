86b09ac8614f584aecb91f7ccf4d00f5765e3df0
16633 Bots in Black <maristrobotics16633@gmail.com>
2020-11-27 11:03:44 -0500

    Added Auto Tag for Paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmodes/SamplePathAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmodes/SamplePathAutonomous.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.teamcode.robot.opmodes;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+@Autonomous(group = "Sample")
 public class SamplePathAutonomous extends LinearOpMode {
 
     @Override
