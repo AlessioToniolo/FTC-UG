35cc7b817f34ecdd35fc1f4205d7acfb5625c2a4
AlessioToniolo <Thegooglemail76!>
2020-11-28 22:07:31 -0500

    Added Auto Tag for Backup Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backup/BackupMecanumAutonomousFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backup/BackupMecanumAutonomousFull.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.backup;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Autonomous(group = "Backup")
 public class BackupMecanumAutonomousFull extends LinearOpMode {
 
     // Motor fields for global use
